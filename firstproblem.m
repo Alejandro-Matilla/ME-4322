6682e3ca239499c6f85b2645df5add1f28e39a2c
Alejandro Matilla <alejandromatillacalvo@gmail.com>
2026-08-25 18:38:12 -0400

    Add files via upload

---
+++ firstproblem.m
@@ -0,0 +1,9 @@
+% Asks the user for two numbers
+firstNumber = input('Enter the first number: ');
+secondNumber = input('Enter the second number: ');
+
+% Add the numbers
+result = firstNumber + secondNumber;
+
+% Display the result
+fprintf('%g + %g = %g\n', firstNumber, secondNumber, result);
